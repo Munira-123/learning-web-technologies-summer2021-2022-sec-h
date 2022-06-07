47ead86d3b841b3c0f8768ba1d49e3f6427cb2b0
Munira-123 <106137352+Munira-123@users.noreply.github.com>
2022-06-07 13:38:49 +0600

    Add files via upload
    Git push

---
+++ lab..php
@@ -0,0 +1,8 @@
+<?php
+function rect_area($length =2,$widtgh =4)
+{
+    $area = $length * $width;
+    echo "Area of Rectangle with length" .$length ."& width ".$width ."is".$area;
+}
+rect_area(); 
+?>
